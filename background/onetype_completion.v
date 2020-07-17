0749cab86c8088121556b0310d6b3227fb62b0db
kalfsvag <kristian.alfsvag@uib.no>
2020-07-17 11:18:33 +0200

    change cquot to N1

--- background/onetype_completion.v
+++ background/onetype_completion.v
@@ -4,10 +4,14 @@ Require Import Category.Core.
 From A_BPQ Require Import quotients categories.
 From A_BPQ Require Export cquot cquot_principles.
 
-(** The definition of the 1type-completion is in the folder cquot *)
+(** The definition of the 1type-completion is in the folder cquot. We change the notation here so that it matches the thesis. *)
+Notation "'N1'" := cquot.
+Notation "'N1_rec'" := cquot_rec.
+Notation "'N1_ind'" := cquot_ind.
+Notation "'N1_ind_set'" := cquot_ind_set.
 
-(* A simplified version of cquot_rec which doesn't need ce  *)
-Definition cquot_rec' {C : PreCategory} (Y : Type)
+(** A simplified version of N1_rec which doesn't need ce  *)
+Definition N1_rec' {C : PreCategory} (Y : Type)
              (cclY : C -> Y)
              (ccleqY : forall {a₁ a₂ : C},
                  C a₁ a₂ -> cclY a₁ = cclY a₂)
@@ -15,9 +19,9 @@ Definition cquot_rec' {C : PreCategory} (Y : Type)
              (cconcatY : forall (a₁ a₂ a₃: C) (c₁: C a₁ a₂) (c₂: C a₂ a₃),
                  ccleqY (c₂ o c₁)%morphism = ccleqY c₁ @ ccleqY c₂)
              {truncY : IsTrunc 1 Y}
-  : cquot C -> Y.
+  : N1 C -> Y.
   Proof.
-    refine (cquot_rec Y cclY ccleqY _ cconcatY).
+    refine (N1_rec Y cclY ccleqY _ cconcatY).
     intro c.
     apply (cancelL (ccleqY c c 1%morphism)).
     refine ((cconcatY c c c _ _)^ @ _).
@@ -26,9 +30,9 @@ Definition cquot_rec' {C : PreCategory} (Y : Type)
   Defined.
 
 
-(** The functor given by the constructors of cquot *)
-Definition include_in_cquot (C : PreCategory)
-  : Functor C (Core.groupoid_category (cquot C)).
+(** The functor given by the constructors of N1 *)
+Definition include_in_N1 (C : PreCategory)
+  : Functor C (Core.groupoid_category (N1 C)).
 Proof.
   srapply @Build_Functor.
   - apply ccl.
@@ -51,12 +55,12 @@ Proof.
 Defined.
 
 (** The universal principle of the 1-type completion *)
-Definition univ_cquot (C : PreCategory) (Y : Type) {istrunc_Y : IsTrunc 1 Y}
-  : (cquot C -> Y) <~> Functor C (Core.groupoid_category Y).
+Definition univ_N1 (C : PreCategory) (Y : Type) {istrunc_Y : IsTrunc 1 Y}
+  : (N1 C -> Y) <~> Functor C (Core.groupoid_category Y).
 Proof.
   srapply @equiv_adjointify.
   - intro f.
-    refine (_ o (include_in_cquot C))%functor.
+    refine (_ o (include_in_N1 C))%functor.
     apply functor_groupoid_category. exact f.
   - intro F.
     srapply @cquot_rec.
@@ -83,13 +87,13 @@ Proof.
 Defined.
 
 
-Definition functor_cquot {C D : PreCategory}
-  : Functor C D -> (cquot C -> cquot D).
+Definition functor_N1 {C D : PreCategory}
+  : Functor C D -> (N1 C -> N1 D).
 Proof.
   intro F.
-  apply (univ_cquot _ _)^-1.
-  refine (include_in_cquot _ o F)%functor.
-  (* srapply @cquot_rec. *)
+  apply (univ_N1 _ _)^-1.
+  refine (include_in_N1 _ o F)%functor.
+  (* srapply @N1_rec. *)
   (* - intro c. apply ccl. exact (F c). *)
   (* - intros c1 c2 f. simpl. *)
   (*   apply ccleq. apply (morphism_of F). exact f. *)
@@ -100,30 +104,30 @@ Proof.
 Defined.
 
 (** Computational rules  *)
-Definition univ_cquot_beta_ccleq {C : PreCategory}
+Definition univ_N1_beta_ccleq {C : PreCategory}
            (Y : Type) {istrunc_Y : IsTrunc 1 Y}
            (F : Functor C (Core.groupoid_category Y))
            {c1 c2 : C} (f : morphism C c1 c2)             
-  : ap ((univ_cquot C Y)^-1 F) (ccleq C f) = (morphism_of F f).
+  : ap ((univ_N1 C Y)^-1 F) (ccleq C f) = (morphism_of F f).
 Proof.
-  unfold univ_cquot. simpl.
+  unfold univ_N1. simpl.
   apply cquot_rec_beta_ccleq.
 Defined.             
 
-Definition functor_cquot_beta_ccleq {C D : PreCategory}
+Definition functor_N1_beta_ccleq {C D : PreCategory}
            (F : Functor C D)
            {c1 c2 : C} (f : morphism C c1 c2)
-  : ap (functor_cquot F) (ccleq C f) =
+  : ap (functor_N1 F) (ccleq C f) =
     ccleq D (morphism_of F f).
 Proof.
-  unfold functor_cquot.
-  apply univ_cquot_beta_ccleq.
+  unfold functor_N1.
+  apply univ_N1_beta_ccleq.
 Defined.
 
 
 (** The 1-type completion preserves set truncation.  *)
-Definition pi0_cat_cquot (C : PreCategory) :
-  pi0_cat C <~> Trunc 0 (cquot C).
+Definition pi0_cat_N1 (C : PreCategory) :
+  pi0_cat C <~> Trunc 0 (N1 C).
 Proof.
   srapply @equiv_adjointify.
   - srapply @set_quotient_rec.
